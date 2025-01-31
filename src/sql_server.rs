4781f80ce83c612b75c786acfaef9b6c1d7acfa8
Ryan Greenup <ryanxgreenup@gmail.com>
2025-01-31 17:35:38 +1100

    Add basic postgres logic

--- src/sql_server.rs
+++ src/sql_server.rs
@@ -31,6 +31,8 @@ pub trait DatabaseOperations {
 }
 
 impl DatabaseOperations for SQLServer {
+    /// See connectorx docs for the mssql docstring
+    /// https://sfu-db.github.io/connector-x/databases/mssql.html
     fn new(config: SQLEngineConfig) -> SQLServer {
         // Define the database credentials
         let mut uri = format!(
@@ -186,6 +188,53 @@ impl SQLServer {
         }
     }
 
-
     // TODO Export to DuckDB
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Postgres {
+    pub config: SQLEngineConfig,
+    pub uri_string: String,
+    source_conn: SourceConn,
+}
+
+impl Postgres {
+    /// The URI string used by connectorx
+    /// See the documentation at <https://sfu-db.github.io/connector-x/databases/postgres.html>
+    fn new(config: SQLEngineConfig) -> Self {
+        // Define the database credentials
+        let mut uri = format!(
+            "postgres://{}:{}@{}:{}/{}",
+            config.username, config.password, config.host, config.port, config.database
+        );
+        let source_conn = SourceConn::try_from(uri.as_str()).expect("parse conn str failed");
+        Self {
+            config,
+            uri_string: uri,
+            source_conn,
+        }
+    }
+
+    /// A Query to get all tables
+    fn get_query_all_tables() -> GetTablesQuery {
+        let column_name = "table_name".into();
+        let query = format!(
+            // \dt
+            r#"
+        SELECT {}
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+        AND table_type = 'BASE TABLE';
+        "#,
+            column_name
+        );
+
+        GetTablesQuery { query, column_name }
+    }
+
+    // TODO how to implement other methods and remain dry?
+
+
+
 }
