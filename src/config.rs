2207cca50d4469f9fc4a3fb81c650abb060084bf
Ryan Greenup <ryanxgreenup@gmail.com>
2025-01-31 15:37:29 +1100

    refactor into method and create struct

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,119 @@
+use connectorx::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
+
+struct DatabaseEngine {
+    pub sql_server: SQLServer,
+    pub postgres: PostgresConfig,
+}
+
+struct GetTablesQuery {
+    /// The query that will return all tables for the given database
+    query: String,
+    /// The column with the table names
+    column_name: String,
+}
+
+#[derive(Debug)]
+pub struct SQLServer {
+    pub config: SqlServerConfig,
+    pub uri_string: String,
+    source_conn: SourceConn,
+}
+
+// TODO this should become a trait
+impl SQLServer {
+    pub fn new(config: SqlServerConfig) -> SQLServer {
+        // Define the database credentials
+        let mut uri = format!(
+            "mssql://{}:{}@{}:{}/{}",
+            config.username, config.password, config.host, config.port, config.database
+        );
+        uri = format!("{uri}?encrypt=false");
+        uri = format!("{uri}&trusted_connection=false");
+        uri = format!("{uri}&trust_server_certificate=true");
+        let source_conn = SourceConn::try_from(uri.as_str()).expect("parse conn str failed");
+        Self {
+            config,
+            uri_string: uri,
+            source_conn,
+        }
+    }
+
+    /// A Query to get all tables
+    fn get_query_all_tables() -> GetTablesQuery {
+        let column_name = "table_name".into();
+        let query = format!(
+            r#"
+        SELECT TABLE_NAME as {}
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_TYPE = 'BASE TABLE' AND
+            TABLE_TYPE != 'scratch';
+        "#,
+            column_name
+        );
+
+        GetTablesQuery { query, column_name }
+    }
+
+    // Returns tables, an empty string indicates a missing
+    pub fn print_tables(&self) {
+        for table in self.get_optional_tables() {
+            if let Some(t) = table {
+                println!("{t}")
+            }
+        }
+    }
+
+    /// Returns tables as optional values
+    pub fn get_optional_tables(&self) -> Vec<Option<String>> {
+        // Some Queries
+        // let queries = &[CXQuery::from("SELECT * FROM Track")];
+
+        // Get the query for all tables
+        let all_tables_query = Self::get_query_all_tables();
+        let query = all_tables_query.query;
+        let colname = all_tables_query.column_name;
+
+        let queries = &[CXQuery::from(&query)];
+
+        // Get a Destination using Arrow
+        let destination = get_arrow(&self.source_conn, None, queries).expect("Run Failed");
+
+        // Get a Dataframe (NOTE must have same polars_core version in connectorx
+        // and polars, look at `cargo tree | grep polars-core`)
+        let data = destination.polars().expect("Unable to get Dataframe");
+
+        // Print the items
+        // TODO we need a struct or Enum
+        let col_of_strings = data
+            .column(&colname)
+            .unwrap_or_else(|e| {
+                panic!("Unable to extract heading {colname} from query:\n{query}\n{e}")
+            })
+            .try_str()
+            .unwrap_or_else(|| {
+                panic!("Unable to parse column {colname} as strings from query:\n{query}")
+            });
+
+        let vec_of_table_names: Vec<Option<String>> = col_of_strings
+            .iter()
+            .map(|item| {
+                if let Some(i) = item {
+                    Some(i.to_string())
+                } else {
+                    eprintln!(
+                        "One of the table names was not found, which is unexpected behaviour"
+                    );
+                    None
+                }
+            })
+            .collect();
+
+        return vec_of_table_names;
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SqlServerConfig {
--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,17 @@ use polars::prelude::ParquetWriter;
 use std::convert::TryFrom;
 mod cli;
 mod config;
-use cli::Cli;
 use clap::Parser;
-use config::Config;
+use cli::Cli;
+use config::{Config, SQLServer};
 use std::process;
-
-fn get_query_all_tables() -> String {
-    return r#"
-    SELECT TABLE_NAME
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_TYPE = 'BASE TABLE';
-    "#
-    .into();
-}
 
 fn main() {
     let cli = Cli::parse();
     let config_path = cli.get_config_path();
-    println!("Using config file at: {}", config_path.display());
 
     match Config::load(&config_path) {
-        Ok(config) => {
-            println!("Config loaded successfully");
-            run(&config.sql_server)
-        }
+        Ok(config) => run(config.sql_server),
         Err(e) => {
             eprintln!("{}", e);
             process::exit(1);
@@ -34,43 +21,7 @@ fn main() {
     }
 }
 
-fn run(sql_config: &config::SqlServerConfig) {
-    // Define the database credentials
-    let mut uri = format!(
-        "mssql://{}:{}@{}:{}/{}",
-        sql_config.username,
-        sql_config.password,
-        sql_config.host,
-        sql_config.port,
-        sql_config.database
-    );
-    uri = format!("{uri}?encrypt=false");
-    uri = format!("{uri}&trusted_connection=false");
-    uri = format!("{uri}&trust_server_certificate=true");
-
-    // Try to make the connection
-    let source_conn = SourceConn::try_from(uri.as_str()).expect("parse conn str failed");
-
-    // Some Queries
-    // let queries = &[CXQuery::from("SELECT * FROM Track")];
-    let queries = &[CXQuery::from(get_query_all_tables().as_str())];
-
-    // This is the data
-    let destination = get_arrow(&source_conn, None, queries).expect("Run Failed");
-    // let data = destination.arrow();
-    // TODO Make this a function so we can loop with a log
-    let mut data = destination.polars().expect("Unable to get Dataframe");
-
-    // print it I guess
-    println!("{:#?}", data);
-
-    let path = "./table_list.parquet";
-    let mut file = std::fs::File::create(path).expect("Unable to create parquet file");
-    ParquetWriter::new(&mut file)
-        .finish(&mut data)
-        .expect("Unable to write parquet file");
-
-    println!("Export Successful!");
-    println!("See Output at {path}");
-    // From here read with duckdb or polars
+fn run(sql_config: config::SqlServerConfig) {
+    let ms_db = SQLServer::new(sql_config);
+    ms_db.print_tables()
 }
